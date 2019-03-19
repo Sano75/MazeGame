f6335fe2cda7df36b6564feba102ae60f12b3d99
Sano75 <43172203+Sano75@users.noreply.github.com>
2019-03-19 19:31:14 +0100

    Basic Structure
    Basic code

--- MazeGame/src/Tests/testLaser.java
+++ MazeGame/src/Tests/testLaser.java
@@ -1,5 +1,21 @@
 package Tests;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.*;
+
+
 
 public class testLaser {
+	
+	@Test
+	public void LaserOnAndOff() {
+		//code to be added
+		
+	}
+	
+	@Test
+	public void GoThroughLaser() {
+		//Code to be added
+	}
 
 }
