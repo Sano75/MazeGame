efa7e39f415d49fb032b210f412fe6531902cc8f
Gabriel-Tshidimu <46536086+Gabriel-Tshidimu@users.noreply.github.com>
2019-03-25 11:38:30 +0100

    Create testDoor.java

---
+++ MazeGame/src/Tests/testDoor.java
@@ -0,0 +1,23 @@
+package Tests;
+
+import org.junit.Test;
+
+public class testDoor {
+	@Test 
+	
+	public void doorOpen() {
+		// code to be added
+		
+		String theState = "close";
+		String expectedState = "close"
+		Door theDoorState = new Door ();
+		String actualState = theDoorState.
+		
+				assertEquals (theState,expectedState,actualState)
+	}
+	
+	public void doorClose() {
+		// code to be added
+	}
+
+}
