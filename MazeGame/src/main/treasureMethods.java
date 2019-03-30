bed14045c53646d8f3e4220f7b75235e347547a3
maxx <max.kjellman@gmail.com>
2019-03-30 11:11:12 +0100

    new test added and methods fixed

--- MazeGame/src/main/treasureMethods.java
+++ MazeGame/src/main/treasureMethods.java
@@ -1,11 +1,34 @@
 package main;
+
 import main.Treasure;
 
 import java.util.Map;
 
 public class treasureMethods {
 
+	static int playerScore = 0;
 
-	
+	public static Integer incrementScore(int playerY, int playerX, int treasureY, int treasureX) {
+
+		Player player = new Player(playerX, playerY);
+		Treasure treasure = new Treasure(treasureX, treasureY);
+
+		if (player.getX() == treasure.getX() && player.getY() == treasure.getY()) {
+			playerScore++;
+		}
+
+		return playerScore;
+
+	}
+
+	public static boolean openDoor(int currentPlayerScore, int score) {
+
+		if (currentPlayerScore == score) {
+			return true;
+		} else {
+			return false;
+		}
+
+	}
 
 }
