56af96503c555952b324520eb4be7898959cab15
Tom Fjällstrom <tom.fjallstrom@gmail.com>
2019-03-31 18:27:56 +0200

    Gabriel: Door Updated

--- MazeGame/src/main/Door.java
+++ MazeGame/src/main/Door.java
@@ -3,24 +3,44 @@ package main;
 import java.awt.Color;
 import java.awt.Rectangle;
 
-public class Door extends GameObject {	
-	Game game;
-	public  Door(Game gam,int x, int y) {
-		this.game = game;
-		this.x = x * 32;
-		this.y = y * 32;
-		this.type = 4;
-		this.color = Color.green;
-	}
+public class Door extends GameObject{
 
-	@Override
-	public void tick() {
-	}
-	@Override
-	public Rectangle getBounds() {
-		return new Rectangle(this.x,this.y,32,32);
-	}
-	public void doorEnter() {
-		game.stop();
-	}
+    boolean visible = false;
+    Game game;
+    
+    public  Door(Game game,int x, int y) {
+
+
+        this.game = game;
+        this.x = x * 32;
+        this.y = y * 32;
+
+        this.type = 4;
+        this.color = Color.green;
+    }
+    
+    // make a method in Door class when collides with player it closes.
+    
+    public void doorEnter() {
+        
+            if (visible)
+            {
+                game.stop();
+            }
+                
+            
+            }
+    
+    public void visible() {
+        visible = true;
+    }
+
+    @Override
+    public void tick() {
+    }
+    @Override
+    public Rectangle getBounds() {
+        return new Rectangle(this.x,this.y,32,32);
+    }
+    
 }
